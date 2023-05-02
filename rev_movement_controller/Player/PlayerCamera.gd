c8385df15115c25f556acea393a13df62d15a866
Ringtail <29079415+RadicalRingtail@users.noreply.github.com>
2023-05-02 18:11:27 -0400

    added scroll wheel zoom and nodes for ui

--- rev_movement_controller/Player/PlayerCamera.gd
+++ rev_movement_controller/Player/PlayerCamera.gd
@@ -21,25 +21,37 @@ func clamp_zoom_value(zoom_value: float) -> Vector2:
 	return Vector2(new_zoom_value, new_zoom_value)
 
 func _unhandled_input(event) -> void:	
-	if enable_zoom:
-		
-			# handle trackpad scrolling 
+	if enable_zoom:		
+		# handle trackpad scrolling 
 		if event is InputEventPanGesture:
 			# zoom out
 			if event.delta.y > 0:
-				scroll_zoom += zoom_amount
+				scroll_zoom += zoom_amount * 0.2
 				current_zoom = clamp_zoom_value(scroll_zoom)
-				
-				if scroll_zoom > max_zoom:
-					scroll_zoom = max_zoom
 			
 			# zoom in
 			if event.delta.y < 0:
+				scroll_zoom -= zoom_amount * 0.2
+				current_zoom = clamp_zoom_value(scroll_zoom)
+					
+		# handle mouse wheel scrolling
+		if event is InputEventMouseButton and event.is_pressed():
+			
+			# zoom in
+			if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+				scroll_zoom += zoom_amount
+				current_zoom = clamp_zoom_value(scroll_zoom)
+			
+			# zoom out
+			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
 				scroll_zoom -= zoom_amount
 				current_zoom = clamp_zoom_value(scroll_zoom)
 				
-				if scroll_zoom < min_zoom:
-					scroll_zoom = min_zoom
+	if scroll_zoom > max_zoom:
+		scroll_zoom = max_zoom
+		
+	elif scroll_zoom < min_zoom:
+		scroll_zoom = min_zoom
 	
 func _physics_process(delta):
 	zoom = lerp(zoom, current_zoom, 1.0)
